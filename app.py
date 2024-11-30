2bdef55d97334c525da88e4457b9ac70b1492e3b
Tarun Choudhary <tarun1940c@gmail.com>
2024-11-30 16:26:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import tempfile
 
 # Initialize the Flask app and configure the database URI
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://tarun:xXc5Ld9kyVJ75PaZsX9uKJU5iV0WiUhb@dpg-ct4je4jtq21c7393ug4g-a/co12"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = os.urandom(24)
 
